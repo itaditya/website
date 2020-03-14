95040c772e7623fafae161a6ef50eef2c4ad96a7
Aditya Agarwal <adityaa803@gmail.com>
2020-03-14 10:15:59 +0530

    minor fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,7 +11,7 @@ import IconCircleArrow from '../components/IconCircleArrow';
 
 const Home = () => (
   <div
-    className="relative min-h-screen bg-gray-200 px-10 py-8 sm:px-24 lg:px-32 xl:px-48 grid grid-cols-12 col-gap-4"
+    className="relative min-h-screen bg-gray-200 px-10 py-8 sm:px-24 lg:px-32 xl:px-40 grid grid-cols-12 col-gap-4"
     style={{ gridTemplateRows: 'auto 1fr auto' }}
   >
     <Head>
@@ -46,7 +46,8 @@ const Home = () => (
             Hey, I'm <span className="text-gray-900 underline">Aditya.</span>
           </h1>
           <p className="mt-2 text-xl">
-            Currently working at <span className="font-bold text-primary-700">HackerRank</span> as a frontend web developer.
+            Currently working at <span className="font-bold text-primary-700">HackerRank</span> as a frontend web
+            developer.
             <br />I love open-sourcing my knowledge, be it through building projects on GitHub, giving talks or writing
             blogs.
           </p>
@@ -89,7 +90,9 @@ const Home = () => (
             <li className="py-2 pl-3 pr-6 rounded-md">
               <a href="/blog" className="flex items-center">
                 <img src="https://via.placeholder.com/20" alt="" className="rounded-full w-10 h-10" />
-                <span className="hover:underline text-2xl text-gray-800 ml-4 max-w-2xl">Escaping from Async/Await Hell</span>
+                <span className="hover:underline text-2xl text-gray-800 ml-4 max-w-2xl">
+                  Escaping from Async/Await Hell
+                </span>
                 <span className="ml-4 text-sm bg-blue-200 rounded-md px-2 text-blue-800 border border-blue-300">
                   20 mins read
                 </span>
@@ -98,24 +101,31 @@ const Home = () => (
             <li className="py-2 pl-3 pr-6 rounded-md mt-4">
               <a href="/blog" className="flex items-center">
                 <img src="https://via.placeholder.com/20" alt="" className="rounded-full w-10 h-10" />
-                <span className="hover:underline text-2xl text-gray-800 ml-4 max-w-2xl">Things I wish I knew before using MongoDB</span>
+                <span className="hover:underline text-2xl text-gray-800 ml-4 max-w-2xl">
+                  Things I wish I knew before using MongoDB
+                </span>
                 <span className="ml-4 text-sm bg-blue-200 rounded-md px-2 text-blue-800 border border-blue-300">
-                  20 mins read
+                  14 mins read
                 </span>
               </a>
             </li>
             <li className="py-2 pl-3 pr-6 rounded-md mt-4">
               <a href="/blog" className="flex items-center">
                 <img src="https://via.placeholder.com/20" alt="" className="rounded-full w-10 h-10" />
-                <span className="hover:underline text-2xl text-gray-800 ml-4 max-w-2xl">Building a game with React Hooks</span>
+                <span className="hover:underline text-2xl text-gray-800 ml-4 max-w-2xl">
+                  Building a game with React Hooks
+                </span>
                 <span className="ml-4 text-sm bg-blue-200 rounded-md px-2 text-blue-800 border border-blue-300">
-                  20 mins read
+                  9 mins read
                 </span>
               </a>
             </li>
           </ul>
         </div>
-        <a href="/blog" className="bg-blue-300 w-56 mt-4 rounded-lg flex flex-col opacity-1 text-center items-center justify-center text-3xl shadow-xl">
+        <a
+          href="/blog"
+          className="bg-blue-300 w-56 mt-4 rounded-lg flex flex-col opacity-1 text-center items-center justify-center text-3xl shadow-xl"
+        >
           <span className="text-4xl text-gray-800">
             <IconCircleArrow />
           </span>
@@ -127,13 +137,17 @@ const Home = () => (
         </a>
       </section>
       <section className="mt-8 text-xl flex">
-        <h1 className="font-bold text-primary-600">Hot off the press:</h1><a href="/blog" className="ml-2 hover:underline">How to do Visual Testing in React</a>
+        <h1 className="font-bold text-primary-600">Hot off the press:</h1>
+        <a href="/blog" className="ml-2 hover:underline">
+          How to do Visual Testing in React
+        </a>
       </section>
       <section className="mt-40 flex flex-col items-center">
         <h2 className="text-4xl font-bold">Open Source Tools</h2>
         <div className="grid grid-cols-4 col-gap-8 row-gap-12 w-full mt-16">
-          {
-            Array(8).fill({}).map(() => (
+          {Array(8)
+            .fill({})
+            .map(() => (
               <article className="bg-white shadow-xl p-4 rounded-lg text-center">
                 <h3 className="w-full text-2xl">Birla</h3>
                 <p className="text-gray-700 mt-3">Generate React components in one command</p>
@@ -143,8 +157,7 @@ const Home = () => (
                   <li className="w-10 h-10 bg-red-500 hover:bg-red-600 rounded-full shadow-md hover:shadow-lg transition duration-200 ease-in"></li>
                 </ul>
               </article>
-            ))
-          }
+            ))}
         </div>
       </section>
     </main>
@@ -157,7 +170,16 @@ const Home = () => (
       </ul>
     </aside>
 
-    <footer className="col-span-12 py-3 bg-gray-400 px-4">footer</footer>
+    <footer className="col-span-12 py-3 px-4 mt-16">
+      <span className="flex items-center">
+        <img
+          src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/120/htc/37/envelope_2709.png"
+          alt="Email Address"
+          className="w-6 h-6"
+        />
+        <span className="ml-2 text-lg">adityaa803@gmail.com</span>
+      </span>
+    </footer>
   </div>
 );
 
