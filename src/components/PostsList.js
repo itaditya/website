7b2072e0150fcac7db66c70e3068f837ef911513
Aditya Agarwal <adityaa803@gmail.com>
2020-03-18 01:54:29 +0530

    tweaks

--- src/components/PostsList.js
+++ src/components/PostsList.js
@@ -8,13 +8,13 @@ export function PostsPopular() {
       <li className="py-2 pl-3 pr-6 rounded-md">
         <Link href="/blog/async-await-hell">
           <a className="flex items-start">
-            <span src="https://via.placeholder.com/20" className="rounded-full text-2xl pt-2" style={{ color: 'hsl(192.7, 63.5%, 54.9%)' }}>
+            <span className="rounded-full text-2xl pt-2" style={{ color: 'hsl(192.7, 63.5%, 54.9%)' }}>
               <IconReact />
             </span>
             <span className="hover:underline text-2xl text-gray-800 ml-4 max-w-2xl">
               Escaping from Async/Await Hell
             </span>
-            <span className="ml-4 text-sm bg-blue-200 rounded-md px-2 text-blue-800 border border-blue-300">
+            <span className="hidden sm:block ml-4 mt-1 text-sm bg-blue-200 rounded-md px-2 text-blue-800 border border-blue-300">
               20 mins read
             </span>
           </a>
@@ -23,13 +23,13 @@ export function PostsPopular() {
       <li className="py-2 pl-3 pr-6 rounded-md mt-4">
         <Link href="/blog/better-components-with-tailwindcss">
           <a className="flex items-start">
-            <span src="https://via.placeholder.com/20" className="rounded-full text-2xl pt-2">
+            <span className="rounded-full text-2xl pt-2">
               <IconReact />
             </span>
             <span className="hover:underline text-2xl text-gray-800 ml-4 max-w-2xl">
               Things I wish I knew before using MongoDB
             </span>
-            <span className="ml-4 text-sm bg-blue-200 rounded-md px-2 text-blue-800 border border-blue-300">
+            <span className="hidden sm:block ml-4 mt-1 text-sm bg-blue-200 rounded-md px-2 text-blue-800 border border-blue-300">
               14 mins read
             </span>
           </a>
@@ -38,13 +38,13 @@ export function PostsPopular() {
       <li className="py-2 pl-3 pr-6 rounded-md mt-4">
         <Link href="/blog/first-post">
           <a className="flex items-start">
-            <span src="https://via.placeholder.com/20" className="rounded-full text-2xl pt-2">
+            <span className="rounded-full text-2xl pt-2">
               <IconReact />
             </span>
             <span className="hover:underline text-2xl text-gray-800 ml-4 max-w-2xl">
               Building a game with React Hooks
             </span>
-            <span className="ml-4 text-sm bg-blue-200 rounded-md px-2 text-blue-800 border border-blue-300">
+            <span className="hidden sm:block ml-4 mt-1 text-sm bg-blue-200 rounded-md px-2 text-blue-800 border border-blue-300">
               9 mins read
             </span>
           </a>
