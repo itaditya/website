7e86d11276567fc615ccbb62b9960509c8fdbc4e
Aditya Agarwal <adityaa803@gmail.com>
2020-03-23 00:59:49 +0530

    temp commit

--- src/pages/api/metrics.js
+++ src/pages/api/metrics.js
@@ -1,9 +1,29 @@
-export default function handler(req, res) {
+import faunadb, { query as q } from 'faunadb';
+
+const { FAUNADB_SECRET } = process.env;
+
+let client;
+
+console.log(FAUNADB_SECRET);
+
+if (secret) {
+  client = new faunadb.Client({ secret: FAUNADB_SECRET });
+}
+
+export default async function handler(req, res) {
   console.log(process.env.GITHUB_TOKEN);
-  console.log(req.query);
   console.log(req.body);
-  res.status(200).json({
-    message: 'success',
-    data: {},
-  });
+
+  try {
+    if (!client) {
+      return [];
+    }
+
+    res.json({
+      message: 'success',
+      data: {},
+    });
+  } catch (error) {
+    res.status(500).json({ error });
+  }
 }
