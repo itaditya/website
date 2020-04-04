51a3f672a93ab1f0eef3daf7ab0815192f2d06ff
Aditya Agarwal <adityaa803@gmail.com>
2020-04-04 18:45:14 +0530

    updated description for seo

--- src/components/SiteHead.js
+++ src/components/SiteHead.js
@@ -5,7 +5,7 @@ export default function SiteHead(props) {
   return (
     <Head>
       <title>{pageName} | Aditya Agarwal</title>
-      <meta name="description" content="Personal Website of a frontend developer working with React.js" />
+      <meta name="description" content="Frontend Developer based in Bangalore with experience of building modern apps with React" />
       <link rel="icon" href="/favicon.ico" />
       <link
         href="https://fonts.googleapis.com/css2?family=Lora:wght@700&family=Roboto:wght@400;500;700&display=swap"
