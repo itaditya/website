6cb17090f2ce181b7111d9ed1c4e89f2bc361059
Aditya Agarwal <adityaa803@gmail.com>
2020-04-26 05:45:40 +0530

    added expand btn

--- src/components/Unpolished.js
+++ src/components/Unpolished.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 
@@ -6,8 +6,40 @@ import SiteHead from '_components/SiteHead';
 import SiteNavbar from '_components/SiteNavbar';
 import SiteFooter from '_components/SiteFooter';
 import { PublishAndReadTime } from '_components/PostExtraInfo';
+import cn from '_utils/classnames';
 
 import renderContentfulMarkup from '_utils/renderContentfulMarkup';
+
+function PostContent(props) {
+  const { post } = props;
+  const [stateExpanded, setStateExpanded] = useState(false);
+  const markup = renderContentfulMarkup(post.content);
+  return (
+    <div data-post-id={post.id} className="py-4 mb-4">
+      <article className="relative px-8 py-5 bg-white rounded-md shadow-lg overflow-hidden">
+        <div className={cn({ 'line-clamp': !stateExpanded })}>
+          <Link href={`?id=${post.id}`}>
+            <a>
+              <h2 className="mt-4 text-2xl text-gray-800 font-heading">{post.title}</h2>
+            </a>
+          </Link>
+          <PublishAndReadTime post={post} />
+          <p className="mt-4 mb-6 text-lg text-gray-600">{post.description}</p>
+          <div dangerouslySetInnerHTML={{ __html: markup }}></div>
+        </div>
+        <div
+          className={cn({ "bottom-0 left-0 flex justify-center w-full": true, "absolute pt-16 pb-4": !stateExpanded })}
+          style={stateExpanded ? {} : { backgroundImage: 'linear-gradient(to bottom, hsla(0, 0%, 100%, 0.1), hsl(0, 0%, 100%))' }}
+        >
+          <button className="px-3 py-1 text-gray-700 hover:bg-gray-200 focus:bg-gray-300 active:bg-gray-300 rounded-md transition ease-in-out duration-200" onClick={() => setStateExpanded((oldState) => !oldState)}>
+            {stateExpanded ? 'Read Less' : 'Read More'}
+          </button>
+        </div>
+        {!stateExpanded && <div className="pb-10"></div>}
+      </article>
+    </div>
+  );
+}
 
 function Unpolished(props) {
   const { posts } = props;
@@ -27,12 +59,12 @@ function Unpolished(props) {
 
   return (
     <div
-      className="grid grid-cols-12 col-gap-4 py-8 bg-gray-200 px:4 sm:px-24 lg:px-32 xl:px-40 font-body min-h-screen"
+      className="grid min-h-screen grid-cols-12 col-gap-4 py-8 bg-gray-200 px:4 sm:px-24 lg:px-32 xl:px-40 font-body"
       style={{ gridTemplateRows: 'auto 1fr auto' }}
     >
       <SiteHead pageName="Unpolished" />
       <SiteNavbar />
-      <aside className="top-0 lg:h-64 col-span-10 col-start-2 pt-8 mt-10 lg:sticky lg:col-start-9 lg:col-span-5 lg:row-start-2 xl:col-start-8 xl:col-span-4">
+      <aside className="top-0 col-span-10 col-start-2 pt-8 mt-10 lg:h-64 lg:sticky lg:col-start-9 lg:col-span-5 lg:row-start-2 xl:col-start-8 xl:col-span-4">
         <h2 className="mb-8 text-4xl font-bold text-gray-700 xl:text-5xl font-heading">Adi's unpolished thoughts...</h2>
         <p className="text-lg text-gray-700 lg:text-xl">
           These posts are my unpolished thoughts. They lack nuance and are not well researched. If I'm wrong about
@@ -49,23 +81,9 @@ function Unpolished(props) {
         </p>
       </aside>
       <main className="col-span-10 col-start-2 py-8 mt-10 lg:col-span-7 lg:row-start-2 xl:col-span-6">
-        {posts.map((post) => {
-          const markup = renderContentfulMarkup(post.content);
-          return (
-            <div key={post.id} data-post-id={post.id} className="py-4 mb-4">
-              <article className="px-8 py-5 bg-white rounded-md shadow-lg">
-                <Link href={`?id=${post.id}`}>
-                  <a>
-                    <h2 className="mt-4 text-2xl text-gray-800 font-heading">{post.title}</h2>
-                  </a>
-                </Link>
-                <PublishAndReadTime post={post} />
-                <p className="mt-4 mb-6 text-lg text-gray-600">{post.description}</p>
-                <div dangerouslySetInnerHTML={{ __html: markup }}></div>
-              </article>
-            </div>
-          );
-        })}
+        {posts.map((post) => (
+          <PostContent key={post.id} post={post} />
+        ))}
       </main>
       <SiteFooter />
     </div>
