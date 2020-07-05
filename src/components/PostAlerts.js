041ad6baf654e41ec48f5351f1f65b1f1720e01b
Aditya Agarwal <adityaa803@gmail.com>
2020-07-05 13:28:53 +0530

    add original publish url

--- src/components/PostAlerts.js
+++ src/components/PostAlerts.js
@@ -1,11 +1,29 @@
 import cn from '_utils/classnames';
 
-export function WarningAlert(props) {
-  const { children, className, ...restProps } = props;
+export function AlertWarning(props) {
+  const { children, className = '', ...restProps } = props;
   const fullClassName = cn({ 'text-center flex justify-center': true, [className]: true });
   return (
     <div role="alert" {...restProps} className={fullClassName}>
       <span className="px-4 py-1 text-yellow-800 bg-orange-200 border-2 border-orange-400 rounded-lg">{children}</span>
+    </div>
+  );
+}
+
+export function AlertOriginalPublish(props) {
+  const { publisherName, href, className = '', ...restProps } = props;
+  const fullClassName = cn({ 'text-gray-700 text-lg text-center flex justify-center space-x-1 mt-8': true, [className]: true });
+  return (
+    <div role="alert" {...restProps} className={fullClassName}>
+      <span>This post was originally published at </span>
+      <a
+        href={href}
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-blue-600 underline hover:text-blue-700 visited:text-indigo-800"
+      >
+        {publisherName}
+      </a>
     </div>
   );
 }
