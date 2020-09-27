84b79263a7e3535824cc48c7d7cf8c4e93d04cb5
Aditya Agarwal <adityaa803@gmail.com>
2020-09-27 11:22:10 +0530

    render images in unpolished thoughts

--- src/utils/renderContentfulMarkup.js
+++ src/utils/renderContentfulMarkup.js
@@ -16,6 +16,12 @@ const optionsContentful = {
       `<ul class="mt-4 ml-8 list-disc text-gray-700 text-base">${next(node.content)}</ul>`,
     [BLOCKS.OL_LIST]: (node, next) =>
       `<ol class="mt-4 ml-8 list-decimal text-gray-700 text-base">${next(node.content)}</ol>`,
+    [BLOCKS.EMBEDDED_ASSET]: (node, next) => {
+      const { fields } = node.data.target;
+      const imageDetails = fields.file.details.image;
+      next(node.content);
+      return `<img class="mt-4 w-full rounded-sm shadow-md" src="${fields.file.url}" alt="${fields.title}" width="${imageDetails.width}" height="${imageDetails.height}" />`;
+    },
     [INLINES.HYPERLINK]: (node, next) => {
       return `<a href=${node.data.uri} class="text-blue-600 underline hover:text-blue-700 visited:text-indigo-800" target="_blank" rel="noopener noreferrer">${next(node.content)}</a>`;
     },
