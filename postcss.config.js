f94c1287ea92b3393123b2b422db7b3be949ace3
Aditya Agarwal <adityaa803@gmail.com>
2020-11-18 16:48:37 +0530

    use latest tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,3 @@
-const purgecssConfig = [
-  '@fullhuman/postcss-purgecss',
-  {
-    // Specify the paths to all of the template files in your project
-    content: ['./src/**/*.js'],
-    // make sure css reset isnt removed on html and body
-    whitelist: ['html', 'body'],
-    // Include any special characters you're using in this regular expression
-    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-  },
-];
-
 module.exports = {
-  plugins: ['tailwindcss', ...(process.env.PURGE_CSS ? [purgecssConfig] : [])],
+  plugins: ['tailwindcss'],
 };
