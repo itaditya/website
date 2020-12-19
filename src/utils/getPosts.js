07e487e9ab33d9d587fcbbe73400376bf6a4277f
Aditya Agarwal <adityaa803@gmail.com>
2020-12-19 18:43:27 +0530

    migrate to import-glob-array

--- src/utils/getPosts.js
+++ src/utils/getPosts.js
@@ -1,61 +1,6 @@
-// This file is auto-generated. Please don't edit it manually.
+import { meta } from '../pages/blog/*.mdx';
+// import { meta } from '_posts/*.mdx';
+
 export default function getPosts() {
-  const filePromise = [];
-
-    filePromise.push(new Promise(resolve => {
-      import('_posts/10-mongo-things-i-wished-i-knew.mdx').then(data => {
-        resolve(data.meta);
-      });
-    }))
-  
-    filePromise.push(new Promise(resolve => {
-      import('_posts/async-await-hell.mdx').then(data => {
-        resolve(data.meta);
-      });
-    }))
-  
-    filePromise.push(new Promise(resolve => {
-      import('_posts/better-components-with-tailwindcss.mdx').then(data => {
-        resolve(data.meta);
-      });
-    }))
-  
-    filePromise.push(new Promise(resolve => {
-      import('_posts/design-systems-react-buttons-base-variant.mdx').then(data => {
-        resolve(data.meta);
-      });
-    }))
-  
-    filePromise.push(new Promise(resolve => {
-      import('_posts/hook-redux-react.mdx').then(data => {
-        resolve(data.meta);
-      });
-    }))
-  
-    filePromise.push(new Promise(resolve => {
-      import('_posts/named-args.mdx').then(data => {
-        resolve(data.meta);
-      });
-    }))
-  
-    filePromise.push(new Promise(resolve => {
-      import('_posts/optimize-your-react-app-with-react-memo.mdx').then(data => {
-        resolve(data.meta);
-      });
-    }))
-  
-    filePromise.push(new Promise(resolve => {
-      import('_posts/react-renderprops-hoc.mdx').then(data => {
-        resolve(data.meta);
-      });
-    }))
-  
-    filePromise.push(new Promise(resolve => {
-      import('_posts/testing-redux-hooked-app.mdx').then(data => {
-        resolve(data.meta);
-      });
-    }))
-  
-return Promise.all(filePromise);
+  return meta;
 }
-
