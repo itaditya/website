76635b2530b1ea7145c6c885ffa93b82f3a803be
Aditya Agarwal <adityaa803@gmail.com>
2021-01-11 03:08:46 +0530

    disable smooth scroll on route transition

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,28 +1,11 @@
-import { useEffect } from 'react';
-import Router from 'next/router';
-
-import { setupMetrics, collectMetrics } from '_utils/tracking';
+import { useMetrics } from '_utils/tracking';
+import { useNormalScrollRoutes } from '_utils/scrollUtils';
 
 import '../css/tailwind.css';
 import '../css/style.css';
 
-const handleRouteChange = url => {
-  collectMetrics({
-    type: 'navigation',
-    path: url,
-    fromPath: Router.pathname,
-  });
-};
-
 export default function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    setupMetrics();
-    collectMetrics({
-      type: 'entry',
-      path: Router.pathname,
-    });
-    Router.events.on('routeChangeStart', handleRouteChange);
-  }, []);
-
+  useMetrics();
+  useNormalScrollRoutes();
   return <Component {...pageProps} />;
 }
